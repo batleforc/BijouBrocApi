6465df7ec6b5090695b8257e3432cc075aca73eb
Maxime Leriche <maxleriche.60@gmail.com>
2021-06-13 18:12:21 +0200

    [add] mise en place test pour le /api

--- config/database.js
+++ config/database.js
@@ -5,15 +5,18 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         host: env("DATABASE_HOST", String(process.env.DB_HOST)),
-        srv: env.bool("DATABASE_SRV", Boolean(process.env.DB_SRV)),
-        port: env.int("DATABASE_PORT", Number(process.env.DB_PORT)),
-        database: env("DATABASE_NAME", String(process.env.DB_NAME)),
+        srv: env.bool("DATABASE_SRV", Boolean(process.env.DB_SRV) || false),
+        port: env.int("DATABASE_PORT", Number(process.env.DB_PORT) || 27017),
+        database: env(
+          "DATABASE_NAME",
+          String(process.env.DB_NAME) || bijoubrocapi
+        ),
         username: env("DATABASE_USERNAME", String(process.env.DB_USERNAME)),
         password: env("DATABASE_PASSWORD", String(process.env.DB_PASSWORD)),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", Boolean(process.env.DB_SSL)),
+        ssl: env.bool("DATABASE_SSL", Boolean(process.env.DB_SSL) || false),
       },
     },
   },
